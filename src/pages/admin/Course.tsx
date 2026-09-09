5776ed75d1914444684422f97c95ed31163fe61d
Limtan-na <tulodchristianjoshua1@gmail.com>
2026-09-10 03:16:16 +0800

    Stack Pre-requisite above Co-requisite in Course form

--- src/pages/admin/Course.tsx
+++ src/pages/admin/Course.tsx
@@ -265,40 +265,38 @@ export default function Course({ profile }: CourseProps) {
                 value={activeForm.title}
                 onChange={(e) => setActiveForm({ ...activeForm, title: e.target.value })} required />
             </label>
-            <div className="create-resource__row">
-              <label className="field">
-                <span>Pre-requisite</span>
-                <select className="input" value={activeForm.prereqNA ? '' : activeForm.prereq}
-                  onChange={(e) => setActiveForm({ ...activeForm, prereq: e.target.value })}
-                  disabled={activeForm.prereqNA}>
-                  <option value="">N/A</option>
-                  {programCourses.map((c) => (
-                    <option key={c.id} value={c.id}>{courseCodeOf(c)}</option>
-                  ))}
-                </select>
-                <label className="na-check">
-                  <input type="checkbox" checked={activeForm.prereqNA}
-                    onChange={(e) => setActiveForm({ ...activeForm, prereqNA: e.target.checked, prereq: e.target.checked ? '' : activeForm.prereq })} />
-                  N/A
-                </label>
+            <label className="field">
+              <span>Pre-requisite</span>
+              <select className="input" value={activeForm.prereqNA ? '' : activeForm.prereq}
+                onChange={(e) => setActiveForm({ ...activeForm, prereq: e.target.value })}
+                disabled={activeForm.prereqNA}>
+                <option value="">N/A</option>
+                {programCourses.map((c) => (
+                  <option key={c.id} value={c.id}>{courseCodeOf(c)}</option>
+                ))}
+              </select>
+              <label className="na-check">
+                <input type="checkbox" checked={activeForm.prereqNA}
+                  onChange={(e) => setActiveForm({ ...activeForm, prereqNA: e.target.checked, prereq: e.target.checked ? '' : activeForm.prereq })} />
+                N/A
               </label>
-              <label className="field">
-                <span>Co-requisite</span>
-                <select className="input" value={activeForm.coreqNA ? '' : activeForm.coreq}
-                  onChange={(e) => setActiveForm({ ...activeForm, coreq: e.target.value })}
-                  disabled={activeForm.coreqNA}>
-                  <option value="">N/A</option>
-                  {programCourses.map((c) => (
-                    <option key={c.id} value={c.id}>{courseCodeOf(c)}</option>
-                  ))}
-                </select>
-                <label className="na-check">
-                  <input type="checkbox" checked={activeForm.coreqNA}
-                    onChange={(e) => setActiveForm({ ...activeForm, coreqNA: e.target.checked, coreq: e.target.checked ? '' : activeForm.coreq })} />
-                  N/A
-                </label>
+            </label>
+            <label className="field">
+              <span>Co-requisite</span>
+              <select className="input" value={activeForm.coreqNA ? '' : activeForm.coreq}
+                onChange={(e) => setActiveForm({ ...activeForm, coreq: e.target.value })}
+                disabled={activeForm.coreqNA}>
+                <option value="">N/A</option>
+                {programCourses.map((c) => (
+                  <option key={c.id} value={c.id}>{courseCodeOf(c)}</option>
+                ))}
+              </select>
+              <label className="na-check">
+                <input type="checkbox" checked={activeForm.coreqNA}
+                  onChange={(e) => setActiveForm({ ...activeForm, coreqNA: e.target.checked, coreq: e.target.checked ? '' : activeForm.coreq })} />
+                N/A
               </label>
-            </div>
+            </label>
             <div className="field">
               <span>Credit</span>
               <div className="create-resource__row">
